cd71636ea06849907a9555c79b754e85790ec40f
SitdePykee <darkgamers751@gmail.com>
2023-10-05 17:00:56 +0700

    Update BCFACTOR.cpp

--- BCFACTOR.cpp
+++ BCFACTOR.cpp
@@ -1,20 +1,26 @@
-#include<bits/stdc++.h>
- 
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int factor(int n){
-	if(n == 0) return 1;
-	else return n * factor(n - 1);
+int factor(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * factor(n - 1);
 }
 
-main(){
-	vector <int> n;
+main()
+{
+	vector<int> n;
 	int a = 1;
-	while(a != 0) {
+	while (a != 0)
+	{
 		cin >> a;
 		n.push_back(a);
 	}
-	for(int i = 0; i < n.size() - 1; i++){
+	for (int i = 0; i < n.size() - 1; i++)
+	{
 		cout << factor(n[i]) << endl;
 	}
 }
